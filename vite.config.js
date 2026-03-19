eef8bee32d115ddafb1c3b100acccc961fa32513
rovinteh <rovinteh0526@gmail.com>
2026-03-20 00:10:58 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative path for universal local and host compatibility
+  base: '/e-commerce/', // Use relative path for universal local and host compatibility
 })
